f533ebeb981b16fa5fb8e031d8816065ea7e0775
Chris Weber <chris963@gmail.com>
2013-07-06 21:03:04 +0200

    harvester update

--- twitter_harvester.js
+++ twitter_harvester.js
@@ -1,3 +1,25 @@
+/**
+ * twitter harvester wrapper
+ * 
+ * MIT Licensed, see License.txt
+ * 
+ */
+
+// utilities module
+var utilities = require('./library/shared/utilities-0.0.1');
+
+// NODE_ENV can be "development", "staging" or "production"
+if (typeof(process.env.NODE_ENV) === 'undefined') {
+    
+    utilities.log('PROCESS ENV NOT FOUND, setting it by default to PRODUCTION', 'red');
+
+    process.env.NODE_ENV = 'production';
+
+}
+
+// get the environment variable
+var environment = process.env.NODE_ENV;
+
 var JamendoFromTwitter = require('./node_modules/jamendo-from-twitter/index.js');
 
 // get configuration
@@ -5,18 +27,18 @@ var configurationModule = require('./application/configurations/configuration.js
 var configuration = configurationModule.get();
 
 // get an harvester
-var harvester = new JamendoFromTwitter(configuration.twitter);
+var harvester = new JamendoFromTwitter(configuration.twitterModule);
 
 // listener
 harvester.on('message', function(message) {
     
-    console.log(message.extracted);
+    utilities.log(message.extracted);
     
 });
 
 var streamOptions = {};
 
-streamOptions.track = 'jamendo OR jamen.do OR jamendomusic OR @jamendo OR @jamen.do OR @jamendomusic OR #jamendo OR #jamen.do OR #jamendomusic';
+streamOptions.track = 'jamendo OR jamen.do OR jamendomusic';
 
 // start harvesting
 harvester.start(streamOptions);
