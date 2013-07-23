7c3d4b0808b162050cab03bfb294d30dbe398f13
Chris Weber <chris963@gmail.com>
2013-07-23 21:31:39 +0200

    harvester debugging

--- twitter_harvester.js
+++ twitter_harvester.js
@@ -6,7 +6,7 @@
  */
 
 // utilities module
-var utilities = require('./library/shared/utilities-0.0.1');
+var utilities = require('./library/shared/utilities-0.0.3');
 
 // NODE_ENV can be "development", "staging" or "production"
 if (typeof(process.env.NODE_ENV) === 'undefined') {
@@ -20,7 +20,17 @@ if (typeof(process.env.NODE_ENV) === 'undefined') {
 // get the environment variable
 var environment = process.env.NODE_ENV;
 
-var JamendoFromTwitter = require('./node_modules/jamendo-from-twitter/index.js');
+try {
+
+    var JamendoFromTwitter = require('./node_modules/jamendo-from-twitter/index.js');
+    
+} catch(exception) {
+    
+    utilities.log(exception);
+    
+}
+
+
 
 // get configuration
 var configurationModule = require('./application/configurations/configuration.js');
@@ -29,16 +39,40 @@ var configuration = configurationModule.get();
 // get an harvester
 var harvester = new JamendoFromTwitter(configuration.twitterModule);
 
-// listener
+// on twitter message listener ("jamendo from twitter" event)
 harvester.on('message', function(message) {
+
+    console.log('harvester got message');
+
+    console.log(message);
+    //utilities.log(message);
+    //utilities.log(message.extracted, 'red', true);
     
-    utilities.log(message.extracted);
-    
+    /**
+        Mongodb twitter tracks log:
+        Jamendo_track_Id
+        Twitter_user_id
+        Twitter_user_name
+        Twitter_tweet_datedate
+        Found_firsttime_date
+        Twitter_Tweet_original_text
+        Twitter_tweet_id (to help avoid duplicates)
+     */
+
 });
 
 var streamOptions = {};
 
-streamOptions.track = 'jamendo OR jamen.do OR jamendomusic';
+//streamOptions.track = 'jamendo OR jamen.do OR jamendomusic';
+streamOptions.track = 'music';
 
 // start harvesting
-harvester.start(streamOptions);
+try {
+    
+    harvester.start(streamOptions);
+    
+} catch(exception) {
+
+    console.log(exception);
+
+}
