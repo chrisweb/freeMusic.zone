9d940a6a9ebb5ff397740be63ec42a36aa927511
chris weber <chris963@gmail.com>
2013-07-29 01:39:01 +0200

    tweets map reduce cron first commit

--- application/models/tweet.js
+++ application/models/tweet.js
@@ -18,21 +18,21 @@ var tweetModel = function(app) {
      * @type tweetsModel.Schema
      */
     var tweetSchema = new Schema({
-        jamendo_track_id: { type: String, trim: true, required: true, index: true },
-        twitter_user_id: { type: String, trim: true, required: true },
-        twitter_user_name: { type: String, trim: true },
-        twitter_user_image: { type: String, trim: true },
+        jamendo_track_id: {type: String, trim: true, required: true, index: true},
+        twitter_user_id: {type: String, trim: true, required: true},
+        twitter_user_name: {type: String, trim: true},
+        twitter_user_image: {type: String, trim: true},
         twitter_tweet_date: Date,
-        twitter_tweet_id: { type: String, trim: true, required: true, index: { unique: true } },
-        twitter_tweet_original_text: { type: String, trim: true }
+        twitter_tweet_id: {type: String, trim: true, required: true, index: {unique: true}},
+        twitter_tweet_original_text: {type: String, trim: true}
     },
-    { safe: true, wtimeout: 10000 }); // return errors and 10 seconds timeout
+    {safe: true, wtimeout: 10000}); // return errors and 10 seconds timeout
 
     // should mongoose checks if indexes exist on every startup?
     tweetSchema.set('autoIndex', true);
-    
+
     this.model = app.mongoose.model(collection, tweetSchema);
-    
+
 };
 
 /**
@@ -42,13 +42,13 @@ var tweetModel = function(app) {
  * @returns {undefined}
  */
 tweetModel.prototype.saveOne = function(data, callback) {
-    
+
     console.log('save a single object');
-    
+
     tweet = new this.model(data);
-    
+
     tweet.save(callback);
-    
+
 };
 
 /**
@@ -58,9 +58,9 @@ tweetModel.prototype.saveOne = function(data, callback) {
  * @returns {undefined}
  */
 tweetModel.prototype.saveAll = function() {
-    
+
     console.log('save multiple objects');
-    
+
 };
 
 /**
@@ -70,9 +70,9 @@ tweetModel.prototype.saveAll = function() {
  * @returns {undefined}
  */
 tweetModel.prototype.deleteOne = function() {
-    
+
     console.log('delete a single object');
-    
+
 };
 
 /**
@@ -82,9 +82,9 @@ tweetModel.prototype.deleteOne = function() {
  * @returns {undefined}
  */
 tweetModel.prototype.deleteAll = function() {
-    
+
     console.log('delete multiple objects');
-    
+
 };
 
 /**
@@ -94,9 +94,9 @@ tweetModel.prototype.deleteAll = function() {
  * @returns {undefined}
  */
 tweetModel.prototype.findOne = function() {
-    
+
     console.log('find a single object');
-    
+
 };
 
 /**
@@ -106,20 +106,55 @@ tweetModel.prototype.findOne = function() {
  * @returns {undefined}
  */
 tweetModel.prototype.findAll = function() {
-    
+
     console.log('find multiple objects');
-    
+
 };
 
-tweetModel.prototype.getPopular = function(options, callback) {
+tweetModel.prototype.mapReduce = function(options, callback) {
+
+    console.log('tweetModel -> mapReduce');
+
+    var mapReduceContainer = {};
     
-    console.log('tweetModel -> getPopular');
+    mapReduceContainer.map = tweetsMap;
+    mapReduceContainer.reduce = tweetsReduce;
     
-    var error = false;
-    var result = {};
-    
-    callback(error, result);
-    
+    tweet = new this.model();
+
+    tweet.mapReduce(mapReduceContainer, callback)
+
 };
+
+var tweetsMap = function() {
+
+    return emit(this.jam_unit + '-' + this.jam_id, this.tw_userid);
+
+}
+
+var tweetsReduce = function(jam_richid, tw_users) {
+
+    jam_richid = jam_richid.split("-");
+
+    var jam_unit = jam_richid[0];
+    var jam_id = jam_richid[1];
+    var reduceVal = {id: jam_id, unit: jam_unit, count_tot: 0, count_uniq: 0, users: []};
+
+    for (var i; i < this.tw_users.length; i++) {
+
+        if (reduceVal.users.indexOf(this.tw_users[i]) == -1) {
+
+            reduceVal.users.push(this.tw_users[i]);
+            reduceVal.count_uniq++;
+
+        }
+
+        reduceVal.count_tot++;
+
+    }
+
+    return reduceVal;
+
+}
 
 module.exports = tweetModel;
