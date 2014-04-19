ac31a05e73e48b6b6d7f55068921f8e5be7f4911
Chris Weber <chris963@gmail.com>
2014-04-19 20:02:08 +0200

    layout template

--- client/desktop_development/scripts/views/layout.js
+++ client/desktop_development/scripts/views/layout.js
@@ -5,6 +5,10 @@ define([
     'templates'
 ], function ($, _, Backbone, JST) {
     'use strict';
+    
+    // https://github.com/gruntjs/grunt-contrib-jst
+    // http://robdodson.me/blog/2012/05/30/using-jquery-deferred-to-load-an-underscore-template/
+    // http://ricostacruz.com/backbone-patterns/#jst_templates
 
     var ApplicationView = Backbone.View.extend({
         template: JST['app/scripts/templates/application.ejs']
